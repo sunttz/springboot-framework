3e6133f48d6c3c3b0e9cbedfc2e80fd995a07b59
suntt <suntt@github.com>
2018-11-28 23:10:19 +0800

    task支持

--- src/main/java/com/stt/springbootframework/SpringbootFrameworkApplication.java
+++ src/main/java/com/stt/springbootframework/SpringbootFrameworkApplication.java
@@ -3,9 +3,15 @@ package com.stt.springbootframework;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @MapperScan("com.stt.springbootframework.dao")
+//开启定时任务
+@EnableScheduling
+//开启异步调用方法
+@EnableAsync
 public class SpringbootFrameworkApplication {
 
     public static void main(String[] args) {
